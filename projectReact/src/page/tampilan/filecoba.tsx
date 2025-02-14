8fb5f2838ab48e4c8f98797dd95c715588432238
RizalNururrahman <RizalNururrahman@gmail.com>
2025-02-14 15:29:52 +0700

    cobaa2

---
+++ projectReact/src/page/tampilan/filecoba.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+
+const Coba: React.FC = () => {
+  return (
+    <div>
+      <p>farel ganteng</p>
+    </div>
+  );
+};
+
+export default Coba;
---
+++ projectReact/src/page/tampilan/filecoba2.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+
+const Coba2: React.FC = () => {
+  return (
+    <div>
+      <p>farel ganteng2</p>
+    </div>
+  );
+};
+
+export default Coba2;
